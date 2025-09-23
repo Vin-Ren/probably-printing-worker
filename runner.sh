2c0ea6375251f2c72df7b8fdc3affd3763e6fd82
Vin-Ren <reaxumrx@gmail.com>
2025-09-23 19:17:37 +0700

    feat: Added runner script

---
+++ runner.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Install dependencies and run the main script
+sudo apt-get update -y
+sudo apt-get install -y libcups2-dev libcairo2 pango1.0-tools libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info fonts-dejavu
+sudo apt-get install -y cups cups-pdf
+
+# Check whether is in a virtual environment
+if [ -z "$VIRTUAL_ENV" ]; then
+    # Not in a virtual environment
+    # Check for virtual env directory
+    directories=("venv" ".venv" "env" ".env")
+    for dir in "${directories[@]}"; do
+        if [ -d "$dir" ]; then
+            echo "Virtual environment found in $dir. Activating..."
+            source "$dir/bin/activate"
+            pip install -r requirements.txt
+            python main.py
+            exit 0
+        fi
+    done
+    echo "Please activate a virtual environment before running this script."
+    echo "Do you want to create and activate a new virtual environment now? (y/n)"
+    read answer
+    if [ "$answer" == "y" ]; then
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install -r requirements.txt
+        echo "Running the main script..."
+        echo "------------------------"
+        python main.py
+    else
+        echo "Exiting. Please activate a virtual environment and try again."
+    exit 1
+    fi
+else
+    python main.py
+fi
+
